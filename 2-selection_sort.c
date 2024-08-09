0535cca39c49c2bd9ce0f8e73b4f09e1413a1e7f
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-08-09 21:07:19 +0000

    finish selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * selection_sort - func
+ * @array: var
+ * @size: var
+ */
+
+void selection_sort(int *array, size_t size)
+{
+ size_t i,j, min;
+  int tmp;
+  for (i = 0; i < size - 1; i++){
+    min = i;
+    for (j = i + 1; j < size; j++){
+      if (array[j] < array[min]){
+        min = j;
+      }
+      if (min != i)
+      {
+        tmp = array[i];
+        array [i] = array[min];
+        array[min] = tmp;
+        print_array(array, size);
+      }
+      }
+    }
+  }
